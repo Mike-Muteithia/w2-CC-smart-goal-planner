991073b08cc4d2a2bc28703cc6a2d891c42e7a8b
Mike Muteithia <mikemuteithia@gmail.com>
2025-07-22 18:03:08 +0300

    Fix error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  basePath: '/w2-CC-smart-goal-planner',
+  assetPrefix: '/w2-CC-smart-goal-planner',
 };
 
 
